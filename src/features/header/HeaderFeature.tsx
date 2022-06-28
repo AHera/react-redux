525d7667af05275566772740eda672d921179aaa
AHera <louriens@gmail.com>
2022-06-28 21:19:52 +0300

    add cart place

--- src/features/header/HeaderFeature.tsx
+++ src/features/header/HeaderFeature.tsx
@@ -1,20 +1,53 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import { MenuNavigation } from "./components";
 
 export const HeaderFeature = () => (
   <header
     css={css`
       background: #000000;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
     `}
   >
     <div
       css={css`
-        max-width: 45px;
-        margin: auto;
+        max-width: 165px;
+        margin-left: 20px;
       `}
     >
       <MenuNavigation />
+    </div>
+    <div
+      css={css`
+        margin-right: 30px;
+        width: 25px;
+        display: flex;
+        flex-wrap: wrap;
+        align-content: center;
+        position: relative;
+      `}
+    >
+      <ShoppingCartIcon
+        css={css`
+          color: #ffffff;
+        `}
+      />
+      <div
+        css={css`
+          background: #ffffff;
+          width: 300px;
+          height: auto;
+          border: 1px solid;
+          position: absolute;
+          top: 25px;
+          right: -30px;
+        `}
+      >
+        123
+      </div>
     </div>
   </header>
 );
--- src/features/home/components/ProductItem.tsx
+++ src/features/home/components/ProductItem.tsx
@@ -1,6 +1,8 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
+import { Button } from "@mui/material";
 import { ProductsInterface } from "../interfaces";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
 interface Props {
   product: ProductsInterface;
@@ -10,15 +12,37 @@ export const ProductItem = ({
   product: { title, description, thumbnail, price },
 }: Props) => (
   <div>
-    <img
+    <div
       css={css`
-        max-width: 228px;
+        min-height: 335px;
+        display: flex;
+        flex-wrap: wrap;
+        align-content: flex-start;
       `}
-      src={thumbnail}
-      alt={title}
-    />
-    <h3>{title}</h3>
-    <p>{description}</p>
-    <p>{price}</p>
+    >
+      <img
+        css={css`
+          max-width: 228px;
+        `}
+        src={thumbnail}
+        alt={title}
+      />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </div>
+    <div
+      css={css`
+        width: 100%;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+        align-items: center;
+      `}
+    >
+      <h4>Price: {price}$</h4>
+      <Button variant="contained" endIcon={<ShoppingCartIcon />}>
+        Buy
+      </Button>
+    </div>
   </div>
 );
